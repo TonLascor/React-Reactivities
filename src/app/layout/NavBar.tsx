91552260cd670d65c6c99e92bb72b1be9ef5047f
tonSan+os <toncsj19@gmail.com>
2026-04-28 19:28:05 -0300

    section 10 - adding errors

--- src/app/layout/NavBar.tsx
+++ src/app/layout/NavBar.tsx
@@ -10,11 +10,11 @@ import { Observer } from "mobx-react-lite";
 
 
 function NavBar() {
-  const {uiStore} = useStore();
+  const { uiStore } = useStore();
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ position: "relative", backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'}}>
+      <AppBar position="static" sx={{ position: "relative", backgroundImage: 'linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)' }}>
         <Container>
           <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <Box>
@@ -36,6 +36,9 @@ function NavBar() {
               <MenuItemLink to='/counter'>
                 Counter
               </MenuItemLink>
+              <MenuItemLink to='/errors'>
+                Errors
+              </MenuItemLink>
             </Box>
             <MenuItem>
               UserMenu
@@ -44,18 +47,18 @@ function NavBar() {
         </Container>
 
         <Observer>
-         {() => uiStore.isLoading ? (
-          <LinearProgress
-            color='secondary'
-            sx={{
-              position:'absolute',
-              bottom: 0,
-              left: 0,
-              right: 0,
-              height: 4
-            }}
-          />
-         ): null} 
+          {() => uiStore.isLoading ? (
+            <LinearProgress
+              color='secondary'
+              sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: 4
+              }}
+            />
+          ) : null}
         </Observer>
 
       </AppBar>
